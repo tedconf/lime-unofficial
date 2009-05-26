15d44eb5638a3415f443cb38d49e18fc575ade60
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-26 19:41:00 +0000

    [lime] fixed the returned value of a test harness when the only errors come from files that return a dubious status
    git-svn-id: http://svn.symfony-project.com/tools/lime/tags/RELEASE_1_0_6@18665 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/lime.php
+++ lib/lime.php
@@ -623,7 +623,7 @@ class lime_harness extends lime_registration
       $this->output->green_bar(sprintf(' Files=%d, Tests=%d', count($this->files), $this->stats['_nb_tests']));
     }
 
-    return $this->stats['_failed_tests'] ? false : true;
+    return $this->stats['_failed_files'] ? false : true;
   }
 
   private function process_test_output($lines)
