59613e23f030512e21b7a98c2a1e1e8532a4b58d
bschussek <bschussek@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-05 20:59:12 +0000

    Adapted conventional test structure
    git-svn-id: http://svn.symfony-project.com/tools/lime/branches/2.0-experimental@19896 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/bin/prove.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the symfony framework.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require_once(dirname(__FILE__).'/../../lib/sfLimeAutoloader.php');
+
+sfLimeAutoloader::register();
+
+$h = new sfLimeHarness(array(
+  'force_colors' => isset($argv) && in_array('--color', $argv),
+  'verbose'      => isset($argv) && in_array('--verbose', $argv),
+));
+$h->base_dir = realpath(dirname(__FILE__).'/..');
+
+foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(dirname(__FILE__).'/../unit'), RecursiveIteratorIterator::LEAVES_ONLY) as $file)
+{
+  if (preg_match('/Test\.php$/', $file))
+  {
+    $h->register($file->getRealPath());
+  }
+}
+
+exit($h->run() ? 0 : 1);
--- tests/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pear run-tests -r `dirname $0`
-if [ -e "./run-tests.log" ]; then
-  exit 1
-fi
