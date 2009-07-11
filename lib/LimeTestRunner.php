619f2727d6837bb62cc68530a4b2d1289b658f5f
bschussek <bschussek@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-11 22:59:16 +0000

    Improved error management and documentation
    git-svn-id: http://svn.symfony-project.com/tools/lime/branches/2.0-experimental@20115 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/LimeTestRunner.php
+++ lib/LimeTestRunner.php
@@ -9,6 +9,28 @@
  * with this source code in the file LICENSE.
  */
 
+/**
+ * Runs a set of test methods.
+ *
+ * You can add different types of callbacks to a test runner. The most important
+ * type is the "test" callback. These callbacks are added by calling addTest().
+ * All "test" callbacks are executed upon calling run().
+ *
+ * The other callback types are called before or after the "test" callbacks:
+ *
+ *    - "before all": Called once before all tests
+ *    - "after all": Called once after all tests
+ *    - "before": Called before each test
+ *    - "after": Called after each test
+ *
+ * These callbacks are added by calling addBeforeAll(), addAfterAll(),
+ * addBefore() and addAfter(). You can add multiple callbacks for each type.
+ * Callbacks are called in the same order in which they are added.
+ *
+ * @package    lime
+ * @author     Bernhard Schussek <bschussek@gmail.com>
+ * @version    SVN: $Id$
+ */
 class LimeTestRunner
 {
   protected
@@ -18,6 +40,9 @@ class LimeTestRunner
     $afterCallbacks     = array(),
     $testCallbacks      = array();
 
+  /**
+   * Runs all registered callbacks.
+   */
   public function run()
   {
     foreach ($this->beforeAllCallbacks as $callback)
@@ -46,29 +71,78 @@ class LimeTestRunner
     }
   }
 
+  /**
+   * Adds a callable that is called once before all tests.
+   *
+   * @param  callable $callback
+   * @throws InvalidArgumentException  If the argument is no callbale
+   */
   public function addBeforeAll($callback)
   {
+    $this->assertIsCallable($callback);
     $this->beforeAllCallbacks[] = $callback;
   }
 
+  /**
+   * Adds a callable that is called once after all tests.
+   *
+   * @param  callable $callback
+   * @throws InvalidArgumentException  If the argument is no callbale
+   */
   public function addAfterAll($callback)
   {
+    $this->assertIsCallable($callback);
     $this->afterAllCallbacks[] = $callback;
   }
 
+  /**
+   * Adds a callable that is called before each test.
+   *
+   * @param  callable $callback
+   * @throws InvalidArgumentException  If the argument is no callbale
+   */
   public function addBefore($callback)
   {
+    $this->assertIsCallable($callback);
     $this->beforeCallbacks[] = $callback;
   }
 
+  /**
+   * Adds a callable that is called after each test.
+   *
+   * @param  callable $callback
+   * @throws InvalidArgumentException  If the argument is no callbale
+   */
   public function addAfter($callback)
   {
+    $this->assertIsCallable($callback);
     $this->afterCallbacks[] = $callback;
   }
 
+  /**
+   * Adds a test callable.
+   *
+   * @param  callable $callback
+   * @throws InvalidArgumentException  If the argument is no callbale
+   */
   public function addTest($callback)
   {
+    $this->assertIsCallable($callback);
     $this->testCallbacks[] = $callback;
+  }
+
+  /**
+   * Asserts that the given argument is a callable.
+   *
+   * @param  mixed $callable
+   * @throws InvalidArgumentException  If the argument is no callbale
+   */
+  private function assertIsCallable($callable)
+  {
+    if (!is_callable($callable))
+    {
+      throw new InvalidArgumentException('The given Argument must be a callable.');
+    }
   }
 
 }
