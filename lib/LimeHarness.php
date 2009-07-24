67c31cd1906558752ac290de78c6379f6c2b302e
bschussek <bschussek@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-24 21:24:16 +0000

    Modified LimeHarness to use the new LimeOutputConsoleSummary
    git-svn-id: http://svn.symfony-project.com/tools/lime/branches/2.0-experimental@20481 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/LimeHarness.php
+++ lib/LimeHarness.php
@@ -14,38 +14,45 @@ class LimeHarness extends LimeRegistration
   protected
     $options    = array(),
     $executable = null,
-    $stats      = array(),
-    $output     = null,
-    $shell      = null;
+    $output     = null;
 
   public function __construct(array $options = array())
   {
     $this->options = array_merge(array(
+      'base_dir'     => null,
       'executable'   => null,
       'force_colors' => false,
       'output'       => null,
-      'shell'        => null,
       'verbose'      => false,
     ), $options);
 
-    $this->output = $this->options['output'] ? $this->options['output'] : new LimeOutput($this->options['force_colors']);
-    $this->shell = $this->options['shell'] ? $this->options['shell'] : new LimeShell($this->options['executable']);
+    $this->options['base_dir'] = realpath($this->options['base_dir']);
+
+    $this->output = $this->options['output'] ? $this->options['output'] : $this->getDefaultOutput($this->options['force_colors']);
   }
 
-  public function toArray()
+  protected function getDefaultOutput($forceColors = false)
   {
-    $results = array();
-    foreach ($this->stats['files'] as $file => $stat)
+    if (in_array('--raw', $GLOBALS['argv']))
     {
-      $results = array_merge($results, $stat['output']);
+      return new LimeOutputRaw();
     }
+    else if (in_array('--xml', $GLOBALS['argv']))
+    {
+      return new LimeOutputXml();
+    }
+    else if (in_array('--array', $GLOBALS['argv']))
+    {
+      $serialize = in_array('--serialize', $GLOBALS['argv']);
 
-    return $results;
-  }
+      return new LimeOutputArray($serialize);
+    }
+    else
+    {
+      $colorizer = LimeColorizer::isSupported() || $forceColors ? new LimeColorizer() : null;
 
-  public function toXml()
-  {
-    return LimeTest::toXml($this->toArray());
+      return new LimeOutputConsoleSummary(new LimePrinter($colorizer), $this->options['base_dir']);
+    }
   }
 
   public function run()
@@ -58,161 +65,16 @@ class LimeHarness extends LimeRegistration
     // sort the files to be able to predict the order
     sort($this->files);
 
-    $this->stats = array(
-      'files'        => array(),
-      'failed_files' => array(),
-      'failed_tests' => 0,
-      'total'        => 0,
-    );
+    $connector = new LimeOutputRawConnector($this->output);
 
     foreach ($this->files as $file)
     {
-      $this->stats['files'][$file] = array();
-      $stats = &$this->stats['files'][$file];
-
-      $relativeFile = $this->getRelativeFile($file);
-      $resultFile = tempnam(sys_get_temp_dir(), 'lime');
-
-      list($return, $output) = $this->shell->execute($file, array('--array', '--serialize'));
-
-      $stats['output'] = $output ? @unserialize($output) : '';
-      if (!$stats['output'])
-      {
-        $stats['output'] = array(array('file' => $file, 'tests' => array(), 'stats' => array('plan' => 1, 'total' => 1, 'failed' => array(0), 'passed' => array(), 'skipped' => array())));
-      }
-      unlink($resultFile);
-
-      $fileStats = &$stats['output'][0]['stats'];
-
-      $delta = 0;
-      if ($return > 0)
-      {
-        $stats['status'] = 'dubious';
-        $stats['status_code'] = $return;
-      }
-      else
-      {
-        $this->stats['total'] += $fileStats['total'];
-
-        if (!$fileStats['plan'])
-        {
-          $fileStats['plan'] = $fileStats['total'];
-        }
-
-        $delta = $fileStats['plan'] - $fileStats['total'];
-        if (0 != $delta)
-        {
-          $stats['status'] = 'dubious';
-          $stats['status_code'] = 255;
-        }
-        else
-        {
-          $stats['status'] = $fileStats['failed'] ? 'not ok' : 'ok';
-          $stats['status_code'] = 0;
-        }
-      }
-
-      $this->output->echoln(sprintf('%s%s%s', substr($relativeFile, -min(67, strlen($relativeFile))), str_repeat('.', 70 - min(67, strlen($relativeFile))), $stats['status']));
-
-      if (0 != $stats['status_code'])
-      {
-        $this->output->echoln(sprintf('    Test returned status %s', $stats['status_code']));
-      }
-
-      if ('ok' != $stats['status'])
-      {
-        $this->stats['failed_files'][] = $file;
-      }
-
-      if ($delta > 0)
-      {
-        $this->output->echoln(sprintf('    Looks like you planned %d tests but only ran %d.', $fileStats['plan'], $fileStats['total']));
-
-        $this->stats['failed_tests'] += $delta;
-        $this->stats['total'] += $delta;
-      }
-      else if ($delta < 0)
-      {
-        $this->output->echoln(sprintf('    Looks like you planned %s test but ran %s extra.', $fileStats['plan'], $fileStats['total'] - $fileStats['plan']));
-      }
-
-      if (false !== $fileStats && $fileStats['failed'])
-      {
-        $this->stats['failed_tests'] += count($fileStats['failed']);
-
-        $this->output->echoln(sprintf("    Failed tests: %s", implode(', ', $fileStats['failed'])));
-      }
+      $connector->connect($file);
     }
 
-    if (count($this->stats['failed_files']))
-    {
-      $format = "%-30s  %4s  %5s  %5s  %s";
-      $this->output->echoln(sprintf($format, 'Failed Test', 'Stat', 'Total', 'Fail', 'List of Failed'));
-      $this->output->echoln("------------------------------------------------------------------");
-      foreach ($this->stats['files'] as $file => $stat)
-      {
-        if (!in_array($file, $this->stats['failed_files']))
-        {
-          continue;
-        }
-        $relativeFile = $this->getRelativeFile($file);
+    $this->output->flush();
 
-        if (isset($stat['output'][0]))
-        {
-          $this->output->echoln(sprintf($format, substr($relativeFile, -min(30, strlen($relativeFile))), $stat['status_code'], count($stat['output'][0]['stats']['failed']) + count($stat['output'][0]['stats']['passed']), count($stat['output'][0]['stats']['failed']), implode(' ', $stat['output'][0]['stats']['failed'])));
-        }
-        else
-        {
-          $this->output->echoln(sprintf($format, substr($relativeFile, -min(30, strlen($relativeFile))), $stat['status_code'], '', '', ''));
-        }
-      }
-
-      $this->output->redBar(sprintf('Failed %d/%d test scripts, %.2f%% okay. %d/%d subtests failed, %.2f%% okay.',
-        $nbFailedFiles = count($this->stats['failed_files']),
-        $nbFiles = count($this->files),
-        ($nbFiles - $nbFailedFiles) * 100 / $nbFiles,
-        $nbFailedTests = $this->stats['failed_tests'],
-        $nbTests = $this->stats['total'],
-        $nbTests > 0 ? ($nbTests - $nbFailedTests) * 100 / $nbTests : 0
-      ));
-
-      if ($this->options['verbose'])
-      {
-        foreach ($this->toArray() as $testSuite)
-        {
-          $first = true;
-          foreach ($testSuite['stats']['failed'] as $testCase)
-          {
-            if (!isset($testSuite['tests'][$testCase]['file']))
-            {
-              continue;
-            }
-
-            if ($first)
-            {
-              $this->output->echoln('');
-              $this->output->error($testSuite['file']);
-              $first = false;
-            }
-
-            $this->output->comment(sprintf('  at %s line %s', $testSuite['tests'][$testCase]['file'], $testSuite['tests'][$testCase]['line']));
-            $this->output->info('  '.$testSuite['tests'][$testCase]['message']);
-            $this->output->echoln($testSuite['tests'][$testCase]['error'], null, false);
-          }
-        }
-      }
-    }
-    else
-    {
-      $this->output->greenBar(' All tests successful.');
-      $this->output->greenBar(sprintf(' Files=%d, Tests=%d', count($this->files), $this->stats['total']));
-    }
-
-    return $this->stats['failed_files'] ? false : true;
-  }
-
-  public function getFailedFiles()
-  {
-    return isset($this->stats['failed_files']) ? $this->stats['failed_files'] : array();
+    // should return correct error value
+    return true;
   }
 }
